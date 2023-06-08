c392915450b2e700b50b34f7017052f66b667c29
pbaille <pierrebaille@gmail.com>
2023-06-08 19:01:17 +0200

    fennel utils improvments

--- src/noon/utils/fennel.clj
+++ src/noon/utils/fennel.clj
@@ -1,20 +1,20 @@
 (ns noon.utils.fennel
-  (:import (java.io ByteArrayOutputStream))
-  (:require [bencode.core :as bc]
-            [clojure.java.shell :refer [sh]]
+  (:require [clojure.java.shell :refer [sh]]
             [backtick :refer [template]]
             [clojure.string :as str]))
 
-(defn compile-string [code-string]
-  (let [{:keys [out err]}
-        (->> (template (let [fennel (require :fennel)
-                             (compiled) (fennel.compile-string ~code-string)]
-                         compiled))
-             (str)
-             (sh "fennel" "-e"))]
-    (if (= "" err)
-      out
-      (throw (Exception. err)))))
+(def compile-string
+  (memoize
+   (fn [code-string]
+     (let [{:keys [out err]}
+           (->> (template (let [fennel (require :fennel)
+                                (compiled) (fennel.compile-string ~code-string)]
+                            compiled))
+                (str)
+                (sh "fennel" "-e"))]
+       (if (= "" err)
+         out
+         (throw (Exception. err)))))))
 
 (defn compile [code]
   (compile-string (str/replace (str code) "," " ")))
