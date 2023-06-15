3701b641f895ad1112fc92d3c9938f3f771c9bbb
pbaille <pierrebaille@gmail.com>
2023-06-15 15:50:46 +0200

    utils to build clojure based keymap
    sending expression to repl via the emacs keybinding system
    
    
    Former-commit-id: 979b26463cadcd2106a995c24aac4615828ef4bd

---
+++ src/noon/utils/cider_keybindings.clj
@@ -0,0 +1,35 @@
+(ns noon.utils.cider-keybindings
+  (:require [backtick :refer [template]]
+            [clojure.string :as str]
+            [clojure.pprint :as pprint]))
+
+(defn all-paths
+      ([m] (all-paths m []))
+      ([x at]
+       (if (map? x)
+         (->> (mapcat (fn [[k v]] (all-paths v [k])) x)
+              (map (fn [[p v]] [(concat at p) v])))
+         [[at x]])))
+
+(defn path->binding-description [p]
+  (str/join "-" (map name p)))
+
+(defn compile-cider-map!
+  [mode-map-sym tree]
+  (let [bindings (all-paths tree)]
+    (with-out-str
+      (clojure.pprint/pprint
+       (template (map! (:map ~mode-map-sym
+                             ~@(mapcat (fn [[path [binding code]]]
+                                         (if binding
+                                           [:desc (path->binding-description path) :n binding
+                                            (template (lambda ()
+                                                              (interactive)
+                                                              (my-cider/eval! ~(str code))))]))
+                                       bindings))))))))
+
+(spit "try-cider-keybindings.el"
+      (compile-cider-map!
+  'my-first-cider-map
+  '{:foo ["a" (+ 1 2)]
+    :bar {:qux ["M-w" (println "M-w")]}}))
