460e72c2fbf02c0755f86dfb67f816b6ab9a7124
pbaille <pierrebaille@gmail.com>
2023-06-28 09:29:40 +0200

    scratch ++
    Former-commit-id: 895476ed65b3fd3e0c471c94b54fdb976b705aad

--- src/noon/scratch.clj
+++ src/noon/scratch.clj
@@ -1,2 +1,12 @@
 (ns noon.scratch
-  (:require [noon.score :as s]))
+  (:use noon.score)
+  (:use noon.lib.reaper0))
+
+(mk (rep 4 d2-))
+
+(tup s1 s2-)
+(play (k (patch :vibraphone)
+    (catn 4 (shuftup d0 d3- d3 d4))
+    (rep 6 (! (one-of d3 d3-
+                      (transpose c3)
+                      (transpose c1-))))))
