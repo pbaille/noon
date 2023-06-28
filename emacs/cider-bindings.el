0a04be8bda8a21e0085f0c06e8aaa4ff204629a1
pbaille <pierrebaille@gmail.com>
2023-06-28 09:28:53 +0200

    compiled bindings files

--- emacs/cider-bindings.el
+++ emacs/cider-bindings.el
@@ -1,46 +1,34 @@
 (map!
  (:map
+  noon-mode-map
+  :desc
+  "score-upd"
+  :n
+  "H-C-u"
+  (lambda
+   ()
+   (interactive)
+   (my-cider/eval!
+    (format "(do (upd-selection! %s) :ok)" (pb/thing-at-point))))))
+
+(map!
+ (:map
   reaper-mode-map
   :desc
-  "focus-closest"
+  "score-upd"
   :n
-  "F"
-  (lambda () (interactive) (my-cider/eval! "(focus-closest!)"))
-  :desc
-  "focus-fw"
-  :n
-  "l"
-  (lambda () (interactive) (my-cider/eval! "(focus-move! 1)"))
-  :desc
-  "focus-bw"
-  :n
-  "h"
-  (lambda () (interactive) (my-cider/eval! "(focus-move! -1)"))
-  :desc
-  "focus-s1"
-  :n
-  "K"
-  (lambda () (interactive) (my-cider/eval! "(focus-upd! noon/s1)"))
-  :desc
-  "focus-s1-"
-  :n
-  "J"
-  (lambda () (interactive) (my-cider/eval! "(focus-upd! noon/s1-)"))
-  :desc
-  "cursor-fw"
-  :n
-  "f"
+  "U"
   (lambda
    ()
    (interactive)
    (my-cider/eval!
-    "(upd-reaper! (fn [{:as reaper, :keys [grid focus]}] (update-in reaper [:focus 0] + grid)))"))
+    (format "(do (reset-score! %s) :ok)" (pb/thing-at-point))))
   :desc
-  "cursor-bw"
+  "selection-upd"
   :n
-  "b"
+  "u"
   (lambda
    ()
    (interactive)
    (my-cider/eval!
-    "(upd-reaper! (fn [{:as reaper, :keys [grid focus]}] (update-in reaper [:focus 0] - grid)))"))))
+    (format "(do (upd-selection! %s) :ok)" (pb/thing-at-point))))))
