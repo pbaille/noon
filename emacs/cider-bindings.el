9a485c097a61e8a0d2cd14e67ccb95d3b8179c7e
pbaille <pierrebaille@gmail.com>
2023-06-30 20:34:45 +0200

    compiled files...

--- emacs/cider-bindings.el
+++ emacs/cider-bindings.el
@@ -31,4 +31,48 @@
    ()
    (interactive)
    (my-cider/eval!
-    (format "(do (upd-selection! %s) :ok)" (pb/thing-at-point))))))
+    (format "(do (upd-selection! %s) :ok)" (pb/thing-at-point))))
+  :desc
+  "focus-d1"
+  :n
+  "M-k"
+  (lambda
+   ()
+   (interactive)
+   (my-cider/eval!
+    (format
+     "(do (upd-focus! noon.score/d1) :ok)"
+     (pb/thing-at-point))))
+  :desc
+  "focus-d1-"
+  :n
+  "M-j"
+  (lambda
+   ()
+   (interactive)
+   (my-cider/eval!
+    (format
+     "(do (upd-focus! noon.score/d1-) :ok)"
+     (pb/thing-at-point))))
+  :desc
+  "focus-s1"
+  :n
+  "M-S-k"
+  (lambda
+   ()
+   (interactive)
+   (my-cider/eval!
+    (format
+     "(do (upd-focus! noon.score/s1) :ok)"
+     (pb/thing-at-point))))
+  :desc
+  "focus-s1-"
+  :n
+  "M-S-j"
+  (lambda
+   ()
+   (interactive)
+   (my-cider/eval!
+    (format
+     "(do (upd-focus! noon.score/s1-) :ok)"
+     (pb/thing-at-point))))))
