47e5b5f0527d4cfb321e124f1b4c7e0762e60a22
pbaille <pierrebaille@gmail.com>
2023-09-23 20:58:49 +0200

    scratch sync
    Former-commit-id: 4a949044e7fb1594d6149fe2c5319b6a08604d0a

--- src/noon/scratch.clj
+++ src/noon/scratch.clj
@@ -1,13 +1,23 @@
 (ns noon.scratch
   (:use noon.score)
-  (:use noon.lib.reaper0))
+  (:use noon.lib.reaper))
 
-(mk (rep 6 d3)
+(play (chans [(patch :clarinet)
+            (cat s1 s2 s3)]
+           [(patch :acoustic-bass)
+            o1- (cat s1 s2 s3)]))
+(lin (rep 6 d3)
     (rep 4 d1-))
 
-(tup s1 s2-)
-(play (k (patch :vibraphone)
-    (catn 4 (shuftup d0 d3- d3 d4))
-    (rep 6 (! (one-of d3 d3-
-                      (transpose c3)
-                      (transpose c1-))))))
+(cat d1 d2)
+(tup d1- d1)
+(chans _ o2-)
+[rev]
+[{:patch :ocarina}]
+
+($ (tup s1 s2-))
+(mk (patch :vibraphone)
+   (catn 4 (shuftup d0 d3- (chans d3 [(patch :marimba) d6]) d4))
+   (rep 6 (! (one-of d3 d3-
+                     (transpose c3)
+                     (transpose c1-)))))
