96f7bb84275d0416d88cbf9dc8b6d542ffa463c8
pbaille <pierrebaille@gmail.com>
2024-06-13 10:30:27 +0200

    utils hydra: add wrap-child wrap-content wrap-head
    Former-commit-id: 1a7d24c317dfbb59b8b46e4278d78588ac480fb8

--- src/noon/utils/emacs/hydra.clj
+++ src/noon/utils/emacs/hydra.clj
@@ -10,13 +10,18 @@
   {:color 'teal})
 
 (do :help
-    (defn symjoin [sep xs] (->> (map name xs) (str/join sep) symbol))
-    (defn pretty-str [x] (with-out-str (pp/pprint x)))
+    (defn symjoin [sep xs]
+      (->> (map name xs) (str/join sep) symbol))
+    (defn pretty-str [x]
+      (with-out-str (pp/pprint x)))
     (defn map->plist [m]
       (mapcat identity m))
-    (defn hydra-sym [segments] (symjoin "/" (cons :noon-hydra segments)))
-    (defn hydra-body-varsym [segments] (symbol (str "#'" (hydra-sym segments) "/body")))
-    (defn hydra-docstring [options] (:hydra/docstring options))
+    (defn hydra-sym [segments]
+      (symjoin "/" (cons :noon-hydra segments)))
+    (defn hydra-body-varsym [segments]
+      (symbol (str "#'" (hydra-sym segments) "/body")))
+    (defn hydra-docstring [options]
+      (:hydra/docstring options))
     (defn options->plist [options]
       (map->plist (select-keys options hydra-option-keys)))
     (defn options->head-plist [options]
@@ -26,63 +31,89 @@
                 [docstring])
               [(list "q" nil "quit" :exit true :column "Nav")]
               (if-let [parent (get path-map (vec (butlast (:hydra/path hydra))))]
-                [(list "ESC" (:hydra/body-varsym parent) (str ".." (clojure.core/name (last (:hydra/path parent)))) :column "Nav")]))))
+                [(list "ESC" (:hydra/body-varsym parent) (str ".." (clojure.core/name (last (:hydra/path parent)))) :column "Nav")])))
+
+    (defn without-qualified-keys [m]
+      (->> (remove (comp qualified-keyword? key) m)
+           (into {}))))
 
 (defn parse-hydra [{:as parent :hydra/keys [path]} [hydra-name key x & xs]]
   (let [[opts body] (if (map? x) [x xs] [{} (cons x xs)])
         path (conj path hydra-name)
+        wrap-self (comp (:hydra/wrap-child parent identity)
+                        (:hydra/wrap-content parent identity))
         base {:hydra/name (hydra-sym path)
               :hydra/key key
               :hydra/path path
               :hydra/hint (or (:hint opts) (name hydra-name))}]
     (if (vector? (first body))
-      (let [options (merge (select-keys (:hydra/options parent) inherited-options) opts)
-            parent (assoc base
-                          :hydra/body-varsym (hydra-body-varsym path)
-                          :hydra/options options
-                          :hydra/docstring (:doc opts)
-                          :hydra/heads (map first body))]
-        (cons parent (mapcat (partial parse-hydra parent) body)))
-      [(assoc base
-              :hydra/options (options->head-plist opts)
-              :hydra/expr (first body))])))
+      (let [options (merge (select-keys (:hydra/options parent) inherited-options)
+                           (without-qualified-keys opts))
+            self (wrap-self (assoc base
+                                   :hydra/body-varsym (hydra-body-varsym path)
+                                   :hydra/options options
+                                   :hydra/docstring (:doc opts)
+                                   :hydra/heads (map first body)))]
+        (cons self (mapcat (partial parse-hydra
+                                    (assoc self
+                                           :hydra/wrap-content (comp (:hydra/wrap-content opts identity)
+                                                                     (:hydra/wrap-content parent identity))
+                                           :hydra/wrap-child (:hydra/wrap-child opts identity)
+                                           :hydra/wrap-head (comp (:hydra/wrap-head opts identity)
+                                                                  (:hydra/wrap-head parent identity))))
+                           body)))
+      [((comp (:hydra/wrap-head parent identity)
+              wrap-self)
+        (assoc base
+               :hydra/options (options->head-plist opts)
+               :hydra/expr (first body)))])))
 
-(defn hydra-path-map [hydras]
-  (reduce (fn [ret h] (assoc ret (:hydra/path h) h))
-          {} hydras))
+(do :compile
 
-(defn hydra-compile-head
-  [{:hydra/keys [key hint expr options body-varsym]}]
-  (if expr
-    (list* key expr hint options)
-    (list key body-varsym hint :column "Sub")))
+    (defn hydra-path-map [hydras]
+      (reduce (fn [ret h] (assoc ret (:hydra/path h) h))
+              {} hydras))
 
-(defn hydra-compile-one [path path-map]
-  (let [{:as hydra :hydra/keys [heads name options]} (get path-map path)]
-    (when heads
-      (list* 'defhydra name (options->plist options)
-             (concat (hydra-body-prelude path-map hydra)
-                     (map (fn [h] (hydra-compile-head (get path-map (conj path h))))
-                          heads))))))
+    (defn hydra-compile-head
+      [{:hydra/keys [key hint expr options body-varsym]}]
+      (if expr
+        (list* key expr hint options)
+        (list key body-varsym hint :column "Sub")))
 
-(defn hydra-compile [spec]
-  (let [hydras (parse-hydra {:hydra/path [] :hydra/options default-hydra-options} spec)
-        path-map (hydra-path-map hydras)]
-    (cons 'progn (keep (fn [[path _]] (hydra-compile-one path path-map)) path-map))))
+    (defn hydra-compile-one [path path-map]
+      (let [{:as hydra :hydra/keys [heads name options]} (get path-map path)]
+        (when heads
+          (list* 'defhydra name (options->plist options)
+                 (concat (hydra-body-prelude path-map hydra)
+                         (map (fn [h] (hydra-compile-head (get path-map (conj path h))))
+                              heads))))))
 
-(comment
+    (defn hydra-compile [spec]
+      (let [hydras (parse-hydra {:hydra/path [] :hydra/options default-hydra-options} spec)
+            path-map (hydra-path-map hydras)]
+        (cons 'progn (keep (fn [[path _]] (hydra-compile-one path path-map)) path-map)))))
+
+(do
+  (require '[noon.utils.misc :refer [template]])
+  (map inc [1 2 3])
   (def sample-hydra-def
-    (template
-     [:root nil
-      {:color teal
-       :doc "this is the root"}
-      [:one "a"
-       [:a "a" (message "one a")]
-       [:b "b" (message "one b")]]
-      [:two "b" [:a "a" (message "two a")]
-       [:b "b" (message "two b")]
-       [:three "c" {:foo bar}
-        [:deep "d" (message "deep")]]]]))
+    (template [:root nil
+               {:color teal
+                :doc "this is the root"
+                :hydra/wrap-head ~(fn [c] (update c :hydra/expr (fn [e] (list 'wrapped e))))}
+               [:one "a"
+                [:a "a" (message "one a")]
+                [:b "b" (message "one b")]]
+               [:two "b"
+                {:hydra/wrap-content ~(fn [c] (assoc c :hydra/pouet :naze))
+                 :hydra/wrap-child ~(fn [c] (assoc c :hydra/q 42))}
+                [:a "a" (message "two a")]
+                [:b "b" (message "two b")]
+                [:three "c"
+                 {:hint "this will be deep"
+                  :foo bar
+                  :hydra/wrap-child ~(fn [c] (update c :hydra/pouet name))}
+                 [:deep "d" (message "deep")]]]]))
 
-  (parse-hydra {:hydra/path [] :hydra/options default-hydra-options} sample-hydra-def)
-  (spit "emacs/compiled/test-hydra.el" (pretty-str (hydra-compile sample-hydra-def))))
+  '(spit "emacs/compiled/test-hydra.el" (pretty-str (hydra-compile sample-hydra-def)))
+  (parse-hydra {:hydra/path [] :hydra/options default-hydra-options} sample-hydra-def))
