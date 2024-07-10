a544386937d6de2fdd8613215a84b7d8acea6777
pbaille <pierrebaille@gmail.com>
2024-07-10 19:45:48 +0200

    clojure babel improvements

--- src/noon/doc/clojure-babel.el
+++ src/noon/doc/clojure-babel.el
@@ -1,12 +1,12 @@
 ;;; -*- lexical-binding: t; -*-
 
 (require 'cider)
-(defun pb-clojure-babel_refresh-dynamic-font-lock-keywords (ns)
-  "Install font-lock rules according to NS.
+(defun pb-clojure-babel_refresh-dynamic-font-lock-keywords (buffer ns)
+  "Install font-lock rules according to NS for BUFFER.
 The *org-src-fontification:clojure-mode* buffer is used to fontify clojure code.
 For blocks to be correctly fontified, we need to install those using cider."
   (with-current-buffer (get-buffer-create
-                        " *org-src-fontification:clojure-mode*")
+                        buffer)
     (setq-local cider-buffer-ns ns)
     (font-lock-remove-keywords nil cider--dynamic-font-lock-keywords)
     (setq-local cider--dynamic-font-lock-keywords
@@ -17,7 +17,18 @@ For blocks to be correctly fontified, we need to install those using cider."
     (font-lock-flush)))
 
 ;; evaluate this to fontify the guide.org buffer
-(pb-clojure-babel_refresh-dynamic-font-lock-keywords "noon.doc.intro")
+(pb-clojure-babel_refresh-dynamic-font-lock-keywords
+ " *org-src-fontification:clojure-mode*"
+ "noon.doc.guide")
+
+(defun pb-org-babel_edit-src-code-hook ()
+  (pb-clojure-babel_refresh-dynamic-font-lock-keywords
+   "*Org Src guide.org[ clojure ]*"
+   "noon.doc.guide")
+  (flycheck-mode -1)
+  (symex-mode-interface))
+
+(advice-add 'org-edit-src-code :after #'pb-org-babel_edit-src-code-hook)
 
 (require 'pb-org-babel)
 
@@ -27,7 +38,7 @@ For blocks to be correctly fontified, we need to install those using cider."
                 (format "((requiring-resolve 'noon.doc.utils/->piano-roll) %s)"
                         content))
      :result (lambda (result)
-               (pr-with-buf
+               (with-current-buffer (get-buffer-create "*pr*")
                 (erase-buffer)
                 (insert (format "'%s" result))
                 (proll-mode 1))
