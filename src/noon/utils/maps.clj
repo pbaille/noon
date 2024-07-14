d201647fd9db6e84412b66e59e6b068f974f27dd
pbaille <pierrebaille@gmail.com>
2024-07-14 17:32:44 +0200

    add docstrings to utils.maps

--- src/noon/utils/maps.clj
+++ src/noon/utils/maps.clj
@@ -1,13 +1,13 @@
 (ns noon.utils.maps
-  "simple maps (nestable) holding data only,
-   nested functions will be interpreted as updates not values"
+  "A namespace to deal with simple maps (nestable) holding data only,
+   nested functions are interpreted as updates not values."
   (:require [noon.utils.misc :as u :refer [f_ defreduction]]
             [noon.utils.chance :as g]))
 
 (declare ++)
 
 (defn value-merge
-  "the function used to merge values of maps"
+  "The function used to merge values of maps"
   [x y]
   (cond (nil? y) x
         (g/gen? x) (value-merge (g/realise x) y)
@@ -17,6 +17,7 @@
         :else y))
 
 (defreduction ++
+  "Deeply merge several maps together using `noon.utils.maps/value-merge`."
   [x y]
   (reduce (fn [m k]
             (let [v (value-merge (get x k) (get y k))]
@@ -26,20 +27,31 @@
           {} (into (set (keys x)) (keys y))))
 
 (defn ->upd
+  "Turns a function or a map into a map update."
   [x]
   (cond (fn? x) x
         (map? x) (f_ (++ _ x))
         :else (u/throw* "mapset/->hm-upd: " x)))
 
 (defreduction upd
+  "Thread `m` through given updates.
+Updates has to be valid arguments to the `noon.utils.maps/->upd` function."
   [m f]
   ((->upd f) m))
 
 (defn check
+  "Check if the result of updating `m` using `b` is a deeply truthy map."
   [m b]
   (u/deep-check (upd m b)))
 
 (defn match
+  "Check if the map `m` is matching with `x`.
+   `x` can either be
+    - a function that,when applied to `m` can return a deeply truthy map indicating a match,
+    - another map from which each subvalues will be match against corresponding subvalues of `m`
+      subvalues of `x` can be regular values that will be check for equality
+      or functions that will be applyed to corresponding subvalues of `m` in order to determine
+      if the match is successful or not."
   [m x]
   (cond (fn? x) (u/deep-check (x m))
         (map? x) (every? (fn [[p v]] (if-let [mv (get-in m p)]
@@ -47,6 +59,7 @@
                          (u/hm-leaves x))))
 
 (defn upd_
+  "Compose several map updates together."
   [& fs]
   (f_ (apply upd _ fs)))
 
