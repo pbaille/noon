004d8f9d554db734a3db06ae7bca1f5a7dbd9caa
pbaille <pierrebaille@gmail.com>
2024-07-18 19:06:08 +0200

    score test WIP

--- test/noon/score_test.clj
+++ test/noon/score_test.clj
@@ -1,8 +1,12 @@
 (ns noon.score-test
   (:require [noon.score :as s]
-            [clojure.test :refer [deftest testing is]]))
+            [clojure.test :refer [deftest testing is]]
+            [noon.utils.pseudo-random :as pr]))
 
 (def E0 s/DEFAULT_EVENT)
+
+(defn E0> [& xs]
+  (s/?reduce #(%2 %1) E0 xs))
 
 (deftest helpers
   (testing "numeric-updates"
@@ -66,10 +70,139 @@
 (deftest event-updates
   (testing "simples"
 
+    (is (= (:duration ((s/dur 2) E0))
+           2))
+    (is (= (:duration ((s/dur 1/2) E0))
+           1/2))
+
     (is (= (:velocity ((s/vel 23) E0))
            23))
     (is (= (:velocity ((s/vel inc) E0))
+           (:velocity ((s/vel+ 1) E0))
            81))
+    (is (= (:velocity ((s/vel- 10) E0))
+           70))
     (is (= (:velocity ((s/vel (s/mul 2)) E0))
            127))
-    (is (zero? (:velocity ((s/vel (s/sub 100)) E0))))))
+    (is (zero? (:velocity ((s/vel (s/sub 100)) E0))))
+
+    (is (zero? (:channel E0)))
+    (is (= (:channel ((s/chan inc) E0))
+           1))
+    (is (= (:channel ((s/chan+ 10) E0))
+           10))
+    (is (= (:channel ((s/chan+ 100) E0))
+           15))
+    (is (= (:channel ((s/chan- 100) E0))
+           0))
+
+    (is (= (:track E0)
+           0))
+    (is (= (:track ((s/track+ 1) E0))
+           1))
+    (is (= (:track ((s/track+ 1000) E0))
+           1000))
+    (is (= (:track ((s/track+ 100000) E0))
+           65535))
+
+    (is (= (:voice E0)
+           0))
+    (is (= (:voice ((s/voice+ 1) E0))
+           1))
+    (is (= (:voice ((s/voice+ 1000) E0))
+           15))
+    (is (= (:voice ((s/voice -1) E0))
+           0))
+
+    (is (= (:cc ((s/cc :volume 10) E0))
+           (:cc ((s/cc "volume" 10) E0))
+           (:cc ((s/cc "Volume" 10) E0))
+           (:cc ((s/cc 'volume 10) E0))
+           (:cc ((s/cc 'Volume 10) E0))
+           {7 10}))
+
+    (is (= (:cc ((s/cc :bank-select-1 10) E0))
+           {0 10}))
+
+    (is (= (:cc ((s/cc :bank-select-1 (s/mul 100))
+                 ((s/cc :bank-select-1 10) E0)))
+           {0 127}))
+
+    (is (= (:cc ((s/cc :bank-select-1 (s/mul -100))
+                 ((s/cc :bank-select-1 10) E0)))
+           {0 0}))
+
+    (is (thrown? Exception (s/cc :vlume 10)))
+
+    (is (= (:patch ((s/patch :vibraphone) E0))
+           (:patch ((s/patch :vibe) E0))
+           [nil 11]))
+
+    (is (= (:patch ((s/patch :chromaphone/smooth-carillon) E0))
+           (:patch ((s/patch :chroma/smooth-carillon) E0))
+           [8 1]))
+
+    (is (= (pr/with-rand 0
+             (:patch ((s/patch :chromaphone/short) E0)))
+           [11 6]))
+
+    (is (= (:patch ((s/patch :truc) E0))
+           [nil 56])))
+
+  (testing "aliases"
+    (is (= (:velocity (s/vel2 E0))
+           21))
+    (is (= (:velocity (s/vel12 E0))
+           127))
+    (is (= (:velocity (s/vel0 E0))
+           0))
+
+    (is (= (:duration (s/dur2 E0))
+           2))
+    (is (= (:duration (s/dur:2 E0))
+           1/2))
+    (is (= (:duration (s/dur5:2 E0))
+           5/2))
+
+    (is (= (:channel (s/chan3 E0))
+           3))
+
+    (is (= (:track (s/track12 E0))
+           12)))
+
+  (testing "pitch"
+
+    ;; see noon.harmony test for wrapped :pitch transformations testing
+
+    (is (= (E0> s/t3 :pitch)
+           (E0> s/s1 s/t3 :pitch)
+           {:scale [0 2 4 5 7 9 11], :struct [0 2 4], :origin {:d 35, :c 60}, :position {:t 3}}))
+
+    (is (= (E0> s/t3- :pitch)
+           {:scale [0 2 4 5 7 9 11], :struct [0 2 4], :origin {:d 35, :c 60}, :position {:t -3}}))
+
+    (is (= (E0> s/s2 :pitch)
+           (E0> s/d1 s/c1 s/s2 :pitch)
+           {:scale [0 2 4 5 7 9 11], :struct [0 2 4], :origin {:d 35, :c 60}, :position {:t 0, :s 2}}))
+
+    (is (= (E0> s/s2- :pitch)
+           {:scale [0 2 4 5 7 9 11], :struct [0 2 4], :origin {:d 35, :c 60}, :position {:t 0, :s -2}}))
+
+    (is (= (E0> s/d1 :pitch)
+           (E0> s/c1 s/d1 :pitch)
+           {:scale [0 2 4 5 7 9 11], :struct [0 2 4], :origin {:d 35, :c 60}, :position {:t 0, :s 0, :d 1}}))
+
+    (is (= (E0> s/d1- :pitch)
+           {:scale [0 2 4 5 7 9 11], :struct [0 2 4], :origin {:d 35, :c 60}, :position {:t 0, :s 0, :d -1}}))
+
+    (is (= (E0> s/c1 :pitch)
+           {:scale [0 2 4 5 7 9 11], :struct [0 2 4], :origin {:d 35, :c 60}, :position {:t 0, :s 0, :d 0, :c 1}}))
+
+    (is (= (E0> s/c1- :pitch)
+           {:scale [0 2 4 5 7 9 11], :struct [0 2 4], :origin {:d 35, :c 60}, :position {:t 0, :s 0, :d 0, :c -1}}))
+
+    (is (= (E0> s/c1- s/o1 :pitch)
+           {:scale [0 2 4 5 7 9 11], :struct [0 2 4], :origin {:d 35, :c 60}, :position {:t 1, :s 0, :d 0, :c -1}}))
+
+    (is (= (E0> s/s1- s/d2 s/o2- :pitch)
+           {:scale [0 2 4 5 7 9 11], :struct [0 2 4], :origin {:d 35, :c 60}, :position {:t -2, :s -1, :d 2}}))))
