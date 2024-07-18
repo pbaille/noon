0fb39aaa0eb338e42454e1badf553729b186ae31
pbaille <pierrebaille@gmail.com>
2024-07-18 19:21:45 +0200

    add some macro to clj-kondo

--- .clj-kondo/noon/score.clj
+++ .clj-kondo/noon/score.clj
@@ -25,3 +25,55 @@
   `(do ~@(mapv (fn [x]
                  (macroexpand `(def ~x nil)))
                xs)))
+
+(defmacro -def-durations []
+  (cons 'do
+        (concat (for [i (range 2 12)]
+                  (list 'do
+                        (list 'def (with-meta (symbol (str "dur" i))
+                                     {:doc (str "Multiply event duration by " i)
+                                      :tags [:event-update :alias :temporal]})
+                              `(noon.score/dur (mul ~i)))
+                        (list 'def (with-meta (symbol (str "dur:" i))
+                                     {:doc (str "Divide event duration by " i)
+                                      :tags [:event-update :alias :temporal]})
+                              `(noon.score/dur (div ~i)))))
+                (for [n (range 2 12)
+                      d (range 2 12)]
+                  (list 'def (with-meta (symbol (str "dur" n ":" d))
+                               {:doc (str "Multiply event duration by " n "/" d)
+                                :tags [:event-update :alias :temporal]})
+                        `(noon.score/dur (mul (/ ~n ~d))))))))
+
+(defmacro -def-velocities []
+  (cons 'do
+        (for [i (range 1 13)]
+          (let [v (int (* i (/ 127 12)))]
+            (list 'def (with-meta (symbol (str "vel" i))
+                         {:doc (str "Set event velocity to " v)
+                          :tags [:event-update :alias]})
+                  `(noon.score/vel ~v))))))
+
+(defmacro -def-channels []
+  (cons 'do
+        (for [i (range 0 16)]
+          (list 'def (with-meta (symbol (str "chan" i))
+                       {:doc (str "Set event midi channel to " i)
+                        :tags [:event-update :alias]})
+                `(noon.score/chan ~i)))))
+
+(defmacro -def-tracks []
+  (cons 'do
+        (for [i (range 0 16)]
+          (list 'def (with-meta (symbol (str "track" i))
+                       {:doc (str "Set event midi channel to " i)
+                        :tags [:event-update :alias]})
+                `(noon.score/track ~i)))))
+
+(defmacro -def-wrapped [wrapper m]
+  (cons 'do (for [[k v] (eval m)]
+              (list 'def
+                    (with-meta (symbol (name k))
+                      {:tags [:event-update :alias :harmonic]
+                       :doc (str "Alias for " (list (symbol "noon.score" (name wrapper)) v))})
+                    (list wrapper v)))))
