2dc5fe054e21f3fbfadac57216a18a09256ae8ee
pbaille <pierrebaille@gmail.com>
2024-07-20 16:33:42 +0200

    freeze score test util

---
+++ test/noon/utils.clj
@@ -0,0 +1,30 @@
+(ns noon.utils
+  (:require [noon.score :as s]
+            [clojure.java.io :as io]))
+
+(defn dir-exists? [dir-name]
+  (let [dir (io/file dir-name)]
+    (and (.exists dir) (.isDirectory dir))))
+
+(defn dir-equal? [dir1 dir2]
+  (let [files1 (sort (next (file-seq (io/file dir1))))
+        files2 (sort (next (file-seq (io/file dir2))))]
+    (and (= (count files1) (count files2))
+         (every? identity (map #(= (slurp %1) (slurp %2)) files1 files2)))))
+
+(def FREEZE_DIR "./test/data/freeze/")
+
+(defn freeze [id score]
+  (let [dir (str FREEZE_DIR id)
+        filename (str dir "/" id)
+        options {:filename filename :midi true}]
+
+    (if (dir-exists? dir)
+      (let [temp-dir (str FREEZE_DIR "temp")]
+        (s/noon (assoc options :filename (str temp-dir "/" id))
+                score)
+        (dir-equal? dir temp-dir))
+      (s/noon options score))))
+
+(comment (freeze "one"
+                 (s/mk (s/cat s/d0 s/d1))))
