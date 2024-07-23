15274d2aeb53956508e98d2bd88a7de22354d595
pbaille <pierrebaille@gmail.com>
2024-07-23 18:24:34 +0200

    rythmn test

---
+++ test/noon/lib/rythmn_test.clj
@@ -0,0 +1,101 @@
+(ns noon.lib.rythmn-test
+  (:use noon.score)
+  (:refer-clojure :exclude [while cat])
+  (:require [noon.test :as t]
+            [noon.lib.rythmn :as r]
+            [clojure.test :refer [deftest testing is]]))
+
+(deftest main
+  (testing "various"
+    (is (t/frozen dur4
+                  dorian
+                  (chans [(patch :vibraphone) vel4 (par> d3 d3 d3)]
+                         [(patch :taiko-drum) (r/gen-tup 9 3 :durations [1 2 3]) ($ (one-of vel4 vel3) (maybe d3 d3-))]
+                         [(patch :acoustic-bass) o1- (r/gen-bintup 9 5)
+                          (parts {:bintup 0} _
+                                 {:bintup 1} ($ (one-of vel0 d3-)))]
+                         [(patch :ocarina)
+                          (r/gen-bintup 27 11  :shifted :euclidean)
+                          ($ (one-of vel0 vel3 vel5 vel7))
+                          (parts {:bintup 0} ($ (one-of d2 d4 d6))
+                                 {:bintup 1} ($ (one-of d3 d5 d7)))])
+                  (append [rev lydian (transpose c4)] [lydian+2 (transpose c4-)] melodic-minor)
+                  (append lydian)
+                  (dup 2)))
+
+    (is (t/frozen (patch :tinkle-bell)
+                  (r/gen-tup 8 5 :durations [2 1/2 1])
+                  (dup 4)))
+
+    (is (t/frozen (patch :tinkle-bell)
+                  dur2
+                  (par [o1- (dupt 2)]
+                       (r/gen-tup 12 5 :durations [2 1/2 1 3])
+                       [o1 (r/gen-tup 12 7 :durations [2 1/2 1 3])])
+                  (dup 4)))
+
+    "shifted"
+    (is (t/frozen (patch :tinkle-bell)
+                  dur2
+                  (par [o1- (dupt 2)]
+                       (r/gen-tup 8 5 :shifted :durations [1/4 1/2 1 2 4]))
+                  (dup 4)))
+
+    (is (t/frozen (patch :tinkle-bell)
+                  dur2
+                  (par [o1- (dupt 2)]
+                       (r/gen-tup 12 5 :shifted)
+                       [o1 (r/gen-tup 12 5 :shifted)])
+                  (dup 4)))
+
+    (is (t/frozen (patch :tinkle-bell)
+                  dur2
+                  (par [o1- (dupt 2)]
+                       (r/gen-tup 12 5 :shifted :durations [1 2 3])
+                       [o1 (r/gen-tup 12 7 :shifted :durations [2 1 3])])
+                  (dup 4)))
+
+    ;; why does this do not freeze correctly ??
+    #_(is (t/frozen {:description "rotation examplee"}
+                  (chans
+                   [(patch :woodblock) o2-]
+                   [(patch :woodblock) (tup dur2 dur3 dur3)
+                    (r/rotation :rand-sub 5)])
+                  (dup 4)))
+
+    (is (t/frozen {:description "rythmic permutation demo"}
+                  (chans
+                ;; beat
+                   [(patch :taiko-drum) vel5 (dup 4)]
+                ;; rythmic permutations
+                   [(patch :woodblock)
+                    (r/sum->tup [2 1 1 1/2 1/2])
+                    ($ (maybe o1 o1-))
+                    (catn 4 (r/permutation 5))]
+                ;; chords
+                   [(patch :electric-piano-1)
+                    o1- vel4 lydian
+                    (par> d0 d3 d3 d3 d3)
+                    (cat (root :C) (root :Eb) (root :Ab) (root :Db))])
+               ;; loop 4
+                  (dup 4)))
+
+    :euclidean
+
+    (is (t/frozen (patch :tinkle-bell)
+                  dur2
+                  (chans o1-
+                         (r/gen-tup 12 5 :euclidean)
+                         [o1 (r/gen-tup 12 7 :euclidean :shifted)])
+                  (dup 4)))
+
+    (is (t/frozen (patch :tinkle-bell)
+                  (let [rtup (! (r/gen-tup 16 5 :euclidean :shifted))]
+                    (chans (tupn 2 o1-)
+                           rtup
+                           [o1 rtup]
+                           [o2 rtup]
+                           #_[oct3 rtup]))
+
+                  (dup 4)
+                  (adjust {:duration 8})))))
