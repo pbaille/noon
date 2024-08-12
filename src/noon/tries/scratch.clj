71d98a5622fb27531666ca871f44aa17102e9e62
pbaille <pierrebaille@gmail.com>
2024-08-12 11:40:16 +0200

    scratch -> examples WIP

--- src/noon/tries/scratch.clj
+++ src/noon/tries/scratch.clj
@@ -7,13 +7,6 @@
             [noon.midi :as midi]
             [clojure.math.combinatorics :as comb]))
 
-(comment :motivation
-
-         (play
-          (scale :phrygian)
-          (tup d0 d1 d2 d4)
-          (append c4 c8)))
-
 (comment
 
   (swap! options* assoc :tracks {0 :chorium})
@@ -21,18 +14,13 @@
   (play vel2 dur2
         (lin* (map reroot [:C :Eb :F# :A]))
         (each (tup (rebase V (structure :sus47))
-                (rebase (scale :lydian) (structure [0 2 3 6]))))
+                   (rebase (scale :lydian) (structure [0 2 3 6]))))
         (chans
          [(each (par s0 s1 s2 s3)) h/voice-led]
          (each [vel0 o1 (ntup> 8 (one-of d1 d1- d3 d3-))])
          (each [(repitch :C-1) t-round]))
 
-        (rep 4 s1))
-
-  (play dur2
-        (chans [(patch :woodblock) (dupt 4)]
-               [(patch :tinkle-bell) (r/gen-tup 12 5 {:shifted true :durations [1 2 3]})])
-        (dup 4)))
+        (rep 4 s1)))
 
 (comment ::m/gen-tup
          (play (scale :melodic-minor)
@@ -40,7 +28,7 @@
          (noon {:play true
                 :tracks {0 :chorium}
                 :filename "./test/data/scratch/1"}
-               (mk dur2
+               (mk dur4
                    (patch :electric-piano-1)
                    (scale :harmonic-minor)
                    (lin I VII I IV I [IIb lydian] V I)
@@ -49,28 +37,11 @@
                    (each (m/gen-tup {:layer :s :length 6 :delta 2 :steps [-2 -1 1 2]}))
                    (each (maybe (par s0 [vel3 s1] [vel3 s2])))
                    (each (superpose [(chan 2) o1 (patch :ocarina)
-                                  (m/gen-tup {:layer :s :length 4 :delta 1 :steps [-1 1 -2 2]})
-                                  (maybe rev)
-                                  (vel-humanize 1/2 [0.1 0.5])])))))
+                                     (m/gen-tup {:layer :s :length 4 :delta 1 :steps [-1 1 -2 2]})
+                                     (maybe rev)
+                                     (vel-humanize 1/2 [0.1 0.5])])))))
 
 (comment :demos
-
-         (do :utils
-
-             (defn fill [dur f]
-               (sf_ (let [sdur (score-duration _)
-                          n (quot sdur dur)]
-                      (assert (zero? (rem sdur dur))
-                              "fill: division length should be a multiple of score length ")
-                      (update-score _ (ntup n f)))))
-
-             (defn fill> [dur f]
-               (sf_ (let [sdur (score-duration _)
-                          n (quot sdur dur)]
-                      (assert (zero? (rem sdur dur))
-                              "fill: division length should be a multiple of score length ")
-                      (println n)
-                      (update-score _ (ntup> n f))))))
 
          (do :roman-degrees-and-more
 
@@ -85,45 +56,10 @@
                    (h/align-contexts :s)
                    (lin s0 s1 s2-)
                    (each [(tup s0 s2)
-                       (each (tup s0 c1- s+ s0))])
+                          (each (tup s0 c1- s+ s0))])
                    (lin same rev))
 
              (swap! options* assoc :tracks {0 :chorium}))
-
-         (do :line-connection
-
-             (do (defn linm [n f]
-                   (rep n [(sf_ #{(assoc (last (sort-by :position _))
-                                         :position 0)})
-                           f]))
-
-                 (defn line [len f]
-                   (sf_ (let [nxt (update-score _ (append [(sf_ #{(assoc (last (sort-by :position _))
-                                                                :position 0)})
-                                                  f]))]
-                          (if (and (not-empty nxt) (<= (count nxt) len))
-                            (recur nxt)
-                            (set (take len (sort-by :position nxt)))))))
-
-                 (defn tupline [len f]
-                   (fit (line len f)))
-
-                 (def s? (one-of s2- s1- s1 s2)))
-
-             (play {:desc "linm demo"}
-                   lydian+
-                   dur8
-                   (lin* (map root [:C :Eb :F# :A]))
-                   (each (fit [(linm 16
-                                  (any-that (within-pitch-bounds? :C-2 :C2)
-                                            (lin s0 c1- s0)
-                                            (lin s0 d1 d1- s0)
-                                            (lin d1 s0)
-                                            (lin s0 d1- d1 s0 s1)
-                                            s1
-                                            s1-))])))
-
-             (midi/stop2))
 
          (do :tries
 
@@ -136,8 +72,8 @@
              (play [lydian+2 sus47]
                    [dur:2 (lin C0 Eb0 F#0 A0)]
                    (each (chans
-                       s0
-                       (shuftup s0 s1 s2 s3)))
+                          s0
+                          (shuftup s0 s1 s2 s3)))
                    (rep 8
                         (parts (chan 0) s1-
                                (chan 1) s1))))
@@ -177,42 +113,19 @@
 
                    (adjust 8))))
 
-(comment
-  (play)
-  (play (lin [I melodic-minor] [VI superlocrian] [VIb lydianb7] [IIb mixolydian])
-        (h/align-contexts :s)
-        (dup 2)
-        (each (chans [(patch :vibraphone) vel6 t0 (par> d0 d3 d3 d3 d3)]
-                  [(patch :acoustic-bass) vel6 t2-]
-                  [(patch :taiko-drum) (tup vel3 vel5 vel0)]
-                  [(ntup> 9 (any-that (within-pitch-bounds? :G-1 :C2)
-                                       d1- d1 d3 d3- d4 d4-))
-                   vel9
-                   (chans (patch :flute)
-                          [o1- vel6 (patch :vibraphone)])]))
-        (lin _ c6)
-        (dup 2)))
-
 (comment :melodic-development
 
-         (defn take-lst [n]
-           (sf_ (let [sorted (sort-by :position _)]
-                  (if (>= (count sorted) n)
-                    (let [taken (take-last n sorted)]
-                      (set (update-score (set taken) {:position (sub (:position (first taken)))})))))))
-
-         (stop)
          (play
           dorian
           dur:7
           (repeat-while (within-time-bounds? 0 8)
-            (append
-             (any-that (within-pitch-bounds? :C0 :C3)
-                       [(take-lst 1) (one-of d1- d1)]
-                       [(take-lst 8) (m/permutation [0 1/4])]
-                       [(take-lst 4) rev]
-                       [(take-lst 4) (m/contour :similar {:extent [-2 2] :layer :d})]))
-            (trim 0 8))
+                        (append
+                         (any-that (within-pitch-bounds? :C0 :C3)
+                                   [(start-from-nth-last 1) (one-of d1- d1)]
+                                   [(start-from-nth-last 8) (m/permutation [0 1/4])]
+                                   [(start-from-nth-last 4) rev]
+                                   [(start-from-nth-last 4) (m/contour :similar {:extent [-2 2] :layer :d})]))
+                        (trim 0 8))
           (each
            (probs {(one-of vel3 vel5 vel7 vel9) 6
                    (superpose [(chan 2) (patch :vibraphone) vel8 (one-of d3 d4)]) 1
@@ -228,12 +141,7 @@
                          [(d-shift -3) (transpose c6)]))
           (append (superpose (k (nlin 4 [(patch :taiko-drum) (chan 3) (! [vel4 (maybe o1- d1) (r/gen-tup 7 3)])])
                                 (dup 8))))
-          #_(start-from 16))
-
-         (play (patch :taiko-drum)
-               (rep 4 o1))
-
-         (stop))
+          #_(start-from 16)))
 
 
 (comment :passings
@@ -276,7 +184,7 @@
                (each (shuftup s0 s1 s2 s3 s4))
                (each (tup _ (one-of s1 s2 s1- s2- s3 s3-)))
                (each (one-of (tup c1- d2 d1 d0)
-                          (tup c1- s1- s0 s2))))
+                             (tup c1- s1- s0 s2))))
 
          (stop)
          (play dur4
@@ -284,32 +192,32 @@
                (each (one-of phrygian6 lydian melodic-minor))
                (dup 2)
                (each (chans [(patch :acoustic-bass) t2- (tup _ s2 s1- _)]
-                         [(patch :flute) vel8]
-                         [(patch :vibraphone) vel4 (par s0 d4 d6 d8 d10 d12)]
-                         [(patch :taiko-drum)
-                          (r/gen-tup 10 4 :euclidean)
-                          (each [(one-of s0 s1 s1-) (one-of vel1 vel3 vel5)])]))
+                            [(patch :flute) vel8]
+                            [(patch :vibraphone) vel4 (par s0 d4 d6 d8 d10 d12)]
+                            [(patch :taiko-drum)
+                             (r/gen-tup 10 4 :euclidean)
+                             (each [(one-of s0 s1 s1-) (one-of vel1 vel3 vel5)])]))
                (parts (chan 1)
                       [(each (shuftup s0 s1 s2 s3 s4))
                        (each (tup _ (one-of s1 s2 s1- s2- s3 s3-)))
                        (each (one-of (tup c1- d2 d1 d0)
-                                  (tup c1- s1- s0 s2)
-                                  (tup c1- s1- s2- s0)))
+                                     (tup c1- s1- s0 s2)
+                                     (tup c1- s1- s2- s0)))
                        (each (one-of vel5 vel6 vel7 vel9))]))
 
          (play melodic-minor
                (shuflin s0 s1 s2 s3)
                (each (let [step (one-of s1 s2 s3 s1- s2- s3-)
-                        ap (lin c1- d1 s1-)]
-                    (tup [_ ap] [step ap] _ step)))
+                           ap (lin c1- d1 s1-)]
+                       (tup [_ ap] [step ap] _ step)))
                (append c2- c2-))
 
          (play melodic-minor
                (lin (shuflin s0 s1 s2 s3)
                     [{:passing true} (shuflin s0 s1 s2 s3)])
                (each (let [step (one-of s1 s2 s3 s1- s2- s3-)
-                        ap (lin c1- d1)]
-                    (tup [_ ap] [step ap] _ step (par s2- s2))))
+                           ap (lin c1- d1)]
+                       (tup [_ ap] [step ap] _ step (par s2- s2))))
                (append c4-)
                (dup 2))
 
@@ -317,19 +225,20 @@
                dur:3
                (shuflin s0 s2 s4)
                (each (one-of (shuftup _ c1- d1)
-                          (shuftup _ d1 d1-)))
+                             (shuftup _ d1 d1-)))
                (m/permutation :rand)
                (rep 3 (one-of (s-shift 1) (s-shift -1)))
                (rep 3 (transpose c3))
                (dup 2))
 
-         (play hungarian
+         (play dorian+4
 
                (lin I IV)
-               ($lin [(shuftup s0 s2 s4)
-                      (one-of (tup c1- _ d1) (tup d1- _ d1))
-                      (m/permutation :rand)
-                      (rep 4 (one-of (s-shift 1) (s-shift -1)))])
+               (m/$lin [(shuftup s0 s2 s4)
+                        ;; (one-of (tup c1- _ d1) (tup d1- _ d1))
+                        (tup c1- _ d1)
+                        (m/permutation :rand)
+                        (rep 4 (one-of (s-shift 1) (s-shift -1)))])
                (append (transpose c3))
                (append (s-shift -1)))
 
@@ -344,9 +253,9 @@
                   c-space (get d-suroundings (case side :up 1 :down 0))
                   step (case side :up 1 :down -1)]
               (update-score _
-                   (if (= c-space 2)
-                     (tup (d-step step) (c-step step) _)
-                     (tup (d-step step) (case side :up c1- :down d1) _))))))
+                            (if (= c-space 2)
+                              (tup (d-step step) (c-step step) _)
+                              (tup (d-step step) (case side :up c1- :down d1) _))))))
 
          (play dur4
                (rup 6 (one-of d4 d3-))
@@ -368,7 +277,7 @@
                (append (transpose c3) (transpose c6))
                (h/align-contexts :s)
                (each (chans [(patch :string-ensemble-1) vel4 (par s2- s0 s2)]
-                         [(patch :ocarina) (shuftup s0 s1 s2) (each (tup c1- [s2- (lin d1 _)] d1 _ s1 s2))])))
+                            [(patch :ocarina) (shuftup s0 s1 s2) (each (tup c1- [s2- (lin d1 _)] d1 _ s1 s2))])))
 
          (let [c-d+ (efn e (if-let [p- (get-in (nh/neibourhood (:pitch e)) [:down :c])]
                              (assoc e :pitch p-)
