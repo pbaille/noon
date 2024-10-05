9105f62d5c827854f9e6901f17300d26533dbaf1
pbaille <pierrebaille@gmail.com>
2024-10-05 18:56:56 +0200

    prepare deployment

---
+++ build.clj
@@ -0,0 +1,24 @@
+(ns build
+  (:require [clojure.tools.build.api :as b]))
+
+(def lib 'pbaille/noon)
+(def version (format "0.1.%s" (b/git-count-revs nil)))
+(def class-dir "target/classes")
+(def jar-file (format "target/%s-%s.jar" (name lib) version))
+
+;; delay to defer side effects (artifact downloads)
+(def basis (delay (b/create-basis {:project "deps.edn"})))
+
+(defn clean [_]
+  (b/delete {:path "target"}))
+
+(defn jar [_]
+  (b/write-pom {:class-dir class-dir
+                :lib lib
+                :version version
+                :basis @basis
+                :src-dirs ["src"]})
+  (b/copy-dir {:src-dirs ["src" "resources"]
+               :target-dir class-dir})
+  (b/jar {:class-dir class-dir
+          :jar-file jar-file}))
