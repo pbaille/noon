41ff32913b8badcb709aa2448c7d17afcba34e45
pbaille <pierrebaille@gmail.com>
2024-12-06 18:13:01 +0100

    remoce uic dep for now

--- client/noon/client/ui.cljs
+++ client/noon/client/ui.cljs
@@ -1,6 +1,5 @@
 (ns noon.client.ui
   (:require [noon.client.eval :as eval]
-            [uic.component :refer [c]]
             [uix.core :as uix :refer [$ defui]]
             ["@uiw/react-codemirror" :default CodeMirror]
             ["@nextjournal/lang-clojure" :refer [clojure]]))
@@ -8,12 +7,12 @@
 
 (defui code-editor [{:keys [source]}]
   (let [[source set-source] (uix/use-state source)]
-    (c :div
+    ($ :div
        ($ CodeMirror
           {:value source
            :on-change (fn [x] (set-source x))
            :extensions #js [(clojure)]})
-       (c :button
+       ($ :button
           {:on-click (fn [_] (eval/evaluate-string (str "(play* " source ")")
                                                    (fn [{:keys [value]}]
                                                      (println value))
