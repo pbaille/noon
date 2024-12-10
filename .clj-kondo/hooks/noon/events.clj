8dd3c27857b1c04f91e46d5cd2e2f95d6f92bd1c
pbaille <pierrebaille@gmail.com>
2024-12-10 09:04:15 +0100

    simplify clj-kondo hooks

--- .clj-kondo/hooks/noon/events.clj
+++ .clj-kondo/hooks/noon/events.clj
@@ -1,5 +1,5 @@
 (ns hooks.noon.events
-  (:require hooks.noon.constants))
+  (:require [hooks.noon.constants :as constants]))
 
 (defmacro import-wrap-harmony-update-constructors [& xs]
   `(do ~@(mapv (fn [x]
@@ -15,66 +15,49 @@
   (cons 'do
         (concat (for [i (range 2 12)]
                   (list 'do
-                        (list 'def (with-meta (symbol (str "dur" i))
-                                     {:doc (str "Multiply event duration by " i)
-                                      :tags [:event-update :alias :temporal]})
+                        (list 'def (symbol (str "dur" i))
                               `(noon.events/dur (noon.numbers/mul ~i)))
-                        (list 'def (with-meta (symbol (str "dur:" i))
-                                     {:doc (str "Divide event duration by " i)
-                                      :tags [:event-update :alias :temporal]})
+                        (list 'def (symbol (str "dur:" i))
                               `(noon.events/dur (noon.numbers/div ~i)))))
                 (for [n (range 2 12)
                       d (range 2 12)]
-                  (list 'def (with-meta (symbol (str "dur" n ":" d))
-                               {:doc (str "Multiply event duration by " n "/" d)
-                                :tags [:event-update :alias :temporal]})
+                  (list 'def (symbol (str "dur" n ":" d))
                         `(noon.events/dur (noon.numbers/mul (/ ~n ~d))))))))
 
 (defmacro -def-velocities []
   (cons 'do
         (for [i (range 1 13)]
           (let [v (int (* i (/ 127 12)))]
-            (list 'def (with-meta (symbol (str "vel" i))
-                         {:doc (str "Set event velocity to " v)
-                          :tags [:event-update :alias]})
+            (list 'def (symbol (str "vel" i))
                   `(noon.events/vel ~v))))))
 
 (defmacro -def-channels []
   (cons 'do
         (for [i (range 0 16)]
-          (list 'def (with-meta (symbol (str "chan" i))
-                       {:doc (str "Set event midi channel to " i)
-                        :tags [:event-update :alias]})
+          (list 'def (symbol (str "chan" i))
                 `(noon.events/chan ~i)))))
 
 (defmacro -def-tracks []
   (cons 'do
         (for [i (range 0 16)]
-          (list 'def (with-meta (symbol (str "track" i))
-                       {:doc (str "Set event midi channel to " i)
-                        :tags [:event-update :alias]})
+          (list 'def (symbol (str "track" i))
                 `(noon.events/track ~i)))))
 
-(defmacro -def-wrapped [wrapper m]
-  (cons 'do (for [[k v] (eval (symbol (str "hooks." (namespace m)) (name m)))]
-              (list 'def
-                    (with-meta (symbol (name k))
-                      {:tags [:event-update :alias :harmonic]
-                       :doc (str "Alias for " (list (symbol "noon.score" (name wrapper)) v))})
-                    (list wrapper v)))))
+(defmacro -def-wrapped [type wrapper]
+  (let [entries (case type
+                  :modes constants/modes
+                  :structures constants/structures
+                  :pitches constants/pitches)]
+    (cons 'do (for [[k v] entries]
+                (list 'def (symbol (name k)) (list (symbol "noon.events" (name wrapper)) v))))))
 
 (defmacro -def-steps [name prefix max f]
   (cons 'do
         (mapcat
          (fn [n]
-           [(list 'def (with-meta (symbol (str prefix n))
-                         {:doc (str "Step up "
-                                    n " " name " " (if (> n 1) "steps" "step") ".")
-                          :tags [:event-update :harmonic]})
+           [(list 'def (symbol (str prefix n))
                   (list f n))
-            (list 'def (with-meta (symbol (str prefix n "-"))
-                         {:doc (str "Step down " n " " name " " (if (> n 1) "steps" "step") ".")
-                          :tags [:event-update :harmonic]})
+            (list 'def (symbol (str prefix n "-"))
                   (list f (list `- n)))])
          (range 1 max))))
 
@@ -82,28 +65,19 @@
   (cons 'do
         (mapcat
          (fn [n]
-           [(list 'def (with-meta (symbol (str prefix n))
-                         {:doc (str "Shift up "
-                                    n " " name (when (> n 1) "s") ".")
-                          :tags [:event-update :harmonic]})
+           [(list 'def (symbol (str prefix n))
                   (list f n))
-            (list 'def (with-meta (symbol (str prefix n "-"))
-                         {:doc (str "Shift down " n " " name (when (> n 1) "s") ".")
-                          :tags [:event-update :harmonic]})
+            (list 'def (symbol (str prefix n "-"))
                   (list f (list `- n)))])
          (range 1 max))))
 
 (defmacro -def-degrees []
   (cons 'do
         (concat (for [[n v] (map vector '[I II III IV V VI VII] (range))]
-                  (list 'def (with-meta n
-                               {:doc (str "Go to degree " n)
-                                :tags [:event-update :harmonic]})
+                  (list 'def n
                         (list 'noon.events/degree v)))
                 (for [[degree-sym degree-val] (map vector '[I II III IV V VI VII] (range))
                       [alteration-sym alteration-val] [["#" 'noon.events/c1] ["b" 'noon.events/c1-]]]
                   (let [[dn dv an av] [degree-sym degree-val alteration-sym alteration-val]]
-                    (list 'def (with-meta (symbol (str dn an))
-                                 {:doc (str "Go to degree " an dn)
-                                  :tags [:event-update :harmonic]})
+                    (list 'def (symbol (str dn an))
                           `[(noon.events/transpose ~av) (noon.events/degree ~dv)]))))))
