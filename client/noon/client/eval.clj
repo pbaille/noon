b34f06ae66ae17e60220e0e912118748e6017ffb
pbaille <pierrebaille@gmail.com>
2024-12-10 13:04:51 +0100

    adapt sci to new ns structure WIP

--- client/noon/client/eval.clj
+++ client/noon/client/eval.clj
@@ -1,43 +1,10 @@
 (ns noon.client.eval
-  (:require [noon.score]
+  (:require [noon.updates]
             [noon.lib.harmony]
             [noon.lib.melody]
             [noon.lib.rythmn]))
 
-(defn get-refered-varsyms []
-  (->> (ns-publics 'noon.score)
-       (map (fn [[n v]] [n (meta v)]))
-       (filter (fn [[_ meta]] (-> (:ns meta) str (= "noon.score"))))
-       (reduce (fn [ret [n meta]]
-                 (let [tags (set (:tags meta))]
-                   (if (seq tags)
-                     (cond (tags :alias) (update ret :aliased conj n)
-                           :else (update ret :refered conj n))
-                     ret)))
-               {:refered [] :aliased []})))
-
-(defmacro user-ns-str []
-  (let [{:keys [refered aliased]} (get-refered-varsyms)]
-    (str `(~'ns noon.client.user
-                (:require [noon.lib.harmony :as ~'h]
-                          [noon.lib.melody :as ~'m]
-                          [noon.lib.rythmn :as ~'r]
-                          [noon.score :as ~'n :refer [~@refered ~@aliased]])))))
-
-(defmacro primitive-map []
-  (let [{:keys [refered aliased]} (get-refered-varsyms)]
-    (into {}
-          (map (fn [s]
-                 [(list 'quote s) (symbol "noon.score" (name s))])
-               (concat refered aliased)))))
-
 (defmacro sci-namespace [ns-sym]
-  (println (->> (ns-publics ns-sym)
-       (map (fn [[n v]] [n (meta v)]))
-       (keep (fn [[n meta]] (let [ns-str (str (:ns meta))]
-                              (when (= ns-str (name ns-sym))
-                                [(list 'quote n) (symbol ns-str (name n))]))))
-       (into {})))
   (->> (ns-publics ns-sym)
        (map (fn [[n v]] [n (meta v)]))
        (keep (fn [[n meta]] (let [ns-str (str (:ns meta))]
--- client/noon/client/eval.cljs
+++ client/noon/client/eval.cljs
@@ -5,13 +5,19 @@
    [noon.lib.melody]
    [noon.lib.rythmn]
    [sci.core :as sci])
-  (:require-macros [noon.client.eval :refer [sci-namespace primitive-map]]))
+  (:require-macros [noon.client.eval :refer [sci-namespace]]))
+
+(def sci-ctx (sci/init {:namespaces {'user (sci-namespace noon.updates)
+                                     'output (sci-namespace noon.output)
+                                     'score (sci-namespace noon.score)
+                                     'h (sci-namespace noon.lib.harmony)
+                                     'm (sci-namespace noon.lib.melody)
+                                     'r (sci-namespace noon.lib.rythmn)}}))
 
 (defn sci-eval [x]
-  (sci/eval-string x {:namespaces {'user (primitive-map)
-                                   'h (sci-namespace noon.lib.harmony)
-                                   'm (sci-namespace noon.lib.melody)
-                                   'r (sci-namespace noon.lib.rythmn)}}))
+  (sci/eval-string sci-ctx x))
+
+(comment (prn (sci/eval-string* sci-ctx "(tup s0 s1 s2 s3)")))
 
 (defn sci-play [x]
-  (sci-eval (str "(play-score (mk " x "))")))
+  (sci-eval (str "(output/play-score (score/mk " x "))")))
