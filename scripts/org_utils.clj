cc3f9c36d75033479da0de69f46baf317e76a3aa
pbaille <pierrebaille@gmail.com>
2024-12-12 14:20:17 +0100

    org-utils WIP create-client-guide-ns

--- scripts/org_utils.clj
+++ scripts/org_utils.clj
@@ -2,7 +2,8 @@
   (:require [clojure.string :as str]
             [zprint.core :as z]
             [clojure.java.io :as io]
-            [clojure.pprint :as pp]))
+            [clojure.pprint :as pp]
+            [me.raynes.fs :as fs]))
 
 "Utils for converting org files to clojure files (regular and test)"
 
@@ -268,20 +269,10 @@
       (org->edn guide-org-str)
       (split-section guide-org-str)))
 
-(defn create-client-guide-ns []
-  (spit "client/noon/client/guide.cljs"
-        (str "(ns noon.client.guide)\n\n"
-             "(def guide \n"
-             (vec (org->edn (slurp "src/noon/doc/guide.org")))
-             ")"))
-  (z/zprint-file "client/noon/client/guide.cljs"
-                 "client/noon/client/guide.cljs"
-                 "client/noon/client/guide.cljs"))
-
 (defn build-all []
   (build-clj-examples)
   (build-clj-guide)
-  (build-examples-tests))    ; replace underscore with space
+  (build-examples-tests)); replace underscore with space
 
 (defn file->title [filename]
   (-> filename
@@ -316,3 +307,51 @@
   (spit "doc/cljdoc.edn"
         (with-out-str (pp/pprint {:cljdoc/languages ["clj"]
                                   :cljdoc.doc/tree [(build-doc-tree "doc/Noon")]}))))
+
+(comment :guide->md->hiccup
+
+         (require '[commonmark-hiccup.core :as h])
+
+         (defmethod h/node-properties org.commonmark.node.IndentedCodeBlock [node]
+           (h/property-map node))
+
+         (def guide-md-filepath "src/noon/doc/guide.md")
+
+         (defn md-str->noon-client-hiccup [md-str]
+           (let [with-h-index (map (fn [[k content :as elem]] (if (and (string? k) (re-matches #"h[1-9]" k))
+                                                                [(Integer/parseInt (subs k 1 2))
+                                                                 content]
+                                                                elem))
+                                   (h/markdown->hiccup md-str))
+                 group-sections (fn self [elems]
+                                  (if-let [[[h & _ :as x] & xs] (seq elems)]
+                                    (cond
+                                      (int? h) (let [take? (fn [[k]] (or (not (int? k)) (< h k)))
+                                                     content (take-while take? xs)
+                                                     remaining (drop-while take? xs)]
+                                                 (cons (concat (list '$ 'noon.client.ui/section
+                                                                     {:level h :title (first (self [(first (second x))]))})
+                                                               (self content))
+                                                       (self remaining)))
+
+                                      (= :pre h) (cons (list '$ 'noon.client.ui/code-editor {:source (str/trim (second (second x)))})
+                                                       (self xs))
+
+                                      (seq? (second x)) (cons (concat (list '$ h) (self (second x)))
+                                                              (self xs))
+                                      (seq? (nth x 2 nil)) (cons (concat (list '$ h (second x))
+                                                                         (self (nth x 2)))
+                                                                 (self xs))
+                                      (vector? x) (cons (cons '$ x) (self xs))
+                                      :else (cons x (self xs)))
+                                    ()))]
+             (vec (group-sections with-h-index))))
+
+         (md-str->noon-client-hiccup (slurp "src/noon/doc/guide.md"))
+
+         (defn create-client-guide-ns []
+           (spit "client/noon/client/guide.cljs"
+                 (str "(ns noon.client.guide (:require [noon.client.ui] [uix.core :refer [$ defui]]))\n\n"
+                      "(defui guide [_]\n  "
+                      (seq (first (md-str->noon-client-hiccup (slurp guide-md-filepath))))
+                      ")"))))
