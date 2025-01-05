51863d7ee9e56f2e0d179e2b2907d669a9b97f2c
pbaille <pierrebaille@gmail.com>
2025-01-05 19:23:46 +0100

    adapt to new :current-path impl

--- client/noon/client/ui/section.cljs
+++ client/noon/client/ui/section.cljs
@@ -10,7 +10,6 @@
   [{:keys [id path idx level title children has-subsections inline-code]}]
 
   (let [header-ref (uix/use-ref)
-        content-ref (uix/use-ref)
         visibility (<< [:doc.ui.folding.get path])
 
         header (ui.utils/level->header-keyword level)
@@ -36,31 +35,24 @@
                                      :summary [expand-button fold-button]
                                      :folded [summary-button]
                                      :expanded [fold-button])
-
         header-visible (hooks/use-visible-intersection
                         header-ref
                         (fn [entry]
                           (.-isIntersecting entry))
                         {:root nil
                          :rootMargin "-55px"
-                         :threshold 0})
-
-        content-visible (hooks/use-visible-intersection
-                         content-ref
-                         (fn [entry]
-                           (.-isIntersecting entry))
-                         {:root nil
-                          :rootMargin "0px"
-                          :threshold 0})]
+                         :threshold 0})]
 
     (uix/use-effect (fn pouet []
-                      (>> [:doc.ui.nodes.set path :idx idx])
-                      (>> [:doc.ui.nodes.set path :header-visible header-visible])
-                      (>> [:doc.ui.nodes.set path :content-visible content-visible]))
-                    [content-visible header-visible path idx])
+                      #_(println "registering node" path)
+                      (>> [:doc.ui.nodes.upd path (fn [node] (merge node {:header-ref header-ref
+                                                                          :header-visible header-visible
+                                                                          :idx idx}))]))
+                    [path idx header-visible])
 
     (c :div.section
-       {:id id}
+       {:id id
+        :on-click (fn [_] (>> [:doc.ui.nodes.pp path]))}
        (c header
           {:ref header-ref
            :style {:flex [:start {:items :baseline :gap 1}]
@@ -71,7 +63,6 @@
           (when right-button (sc button-style right-button)))
 
        (c :div
-          {:ref content-ref
-           :style {:display (if (= :folded visibility) :none :block)
+          {:style {:display (if (= :folded visibility) :none :block)
                    :p [0 0 0 2]}}
           children))))
