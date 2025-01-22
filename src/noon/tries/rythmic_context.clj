adad29cfff1b9760717b3420c4137ed661a730cf
pbaille <pierrebaille@gmail.com>
2025-01-22 18:09:05 +0100

    implement rtup and rlin

--- src/noon/tries/rythmic_context.clj
+++ src/noon/tries/rythmic_context.clj
@@ -1,4 +1,7 @@
-(ns noon.tries.rythmic-context)
+(ns noon.tries.rythmic-context
+  (:require [noon.events :as events]
+            [noon.utils.misc :as u]
+            [noon.score :as score]))
 
 (do :ctx
 
@@ -66,12 +69,6 @@
                           score))
                    (tup [] format))))
 
-    (mapv (fn [score]
-            (set (map #(assoc % :pos-dur (map float (ctx->pos-dur (:time %)))) score)))
-          (tup-score (set (map (fn [ctx] {:time ctx})
-                               (tup [] [1 2 1])))
-                     [1 2 1]))
-
     (defn split-score
       "split `score` into 2 scores.
        the first including events contained in the given `ctx`
@@ -83,7 +80,89 @@
                               score)]
         [(set (grouped false)) (set (grouped true))]))
 
-    (split-score (tup-score (set (map (fn [ctx] {:time ctx})
-                                      (tup [] [1 2 1])))
-                            [1 2 1])
-                 (mk [4 1 2])))
+    (defn ->old-format [score]
+      (set (map (fn [e]
+                  (let [[position duration] (ctx->pos-dur (:time e))]
+                    (assoc e :position position :duration duration)))
+                score)))
+
+    (defn score->timescale
+      "Get the timescale of `score`
+       (every events hold it under the :timescale key)"
+      [score]
+      (:timescale (first score) 1))
+
+    (defn tup-scores
+      "tups `scores` into one"
+      [scores timescale]
+      (set (mapcat (fn [score ctx]
+                     (map (fn [e]
+                            (-> (assoc e :timescale timescale)
+                                (update :time (fn [t] (into ctx t)))))
+                          score))
+                   scores
+                   (tup [] (map score->timescale scores)))))
+
+    (u/defn* rtup
+      "like regular tup but using rythmn-context"
+      [updates]
+      (score/sfn score
+                 (tup-scores (mapv (fn [u] (score/update-score score u))
+                                   updates)
+                             (score->timescale score))))
+
+    (defn rscale
+      "This is not good, scale should be stored at the score lvl..."
+      [ratio]
+      (score/sfn score
+                 (set (map (fn [e] (update e :timescale (fnil * 1) ratio))
+                           score))))
+
+    (u/defn* rlin
+      "like regular lin but using rythmn-context"
+      [updates]
+      (score/sfn score
+                 (let [scores (mapv (fn [u] (score/update-score score u))
+                                    updates)]
+                   (tup-scores scores
+                               (reduce + (map score->timescale scores))))))
+
+    (comment
+      (require '[noon.output :refer [noon play]])
+      (require '[noon.updates :as upds :refer [s0 s1 s2 d0 d1 d2]])
+      (def score0 #{(assoc events/DEFAULT_EVENT
+                           :time [])})
+      (defn p [& xs]
+        (let [score (reduce score/update-score score0 xs)]
+          (noon {:play true
+                 :bpm (/ 60 (score->timescale score))}
+                (->old-format score))))
+
+      (p (rlin s0 [s1 (rscale 2)] s2)
+         (rtup d0 d1 d2))
+      (play (upds/lin s0 [upds/dur2 s1] s2)
+            (upds/tup d0 d1 d2)))
+
+    (comment
+      (def score0 #{(assoc events/DEFAULT_EVENT
+                           :time [])})
+
+      (noon.output/noon {:play true
+                         :bpm 10}
+                        (-> score0
+                            (tup-score [1 2 1])
+                            (tup-score [1 1 2])
+                            (tup-score [1 1])
+                            ->old-format)))
+
+    (comment
+      (mapv (fn [score]
+              (set (map #(assoc % :pos-dur (map float (ctx->pos-dur (:time %)))) score)))
+            (tup-score (set (map (fn [ctx] {:time ctx})
+                                 (tup [] [1 2 1])))
+                       [1 2 1]))
+
+      (split-score (tup-score (set (map (fn [ctx] {:time ctx})
+                                        (tup [] [1 2 1])))
+                              [1 2 1])
+                   (mk [4 1 2]))))
