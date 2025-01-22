55d86fb0d097dfd1b7f11a7b5b96a3eb28d1ff95
pbaille <pierrebaille@gmail.com>
2025-01-22 12:35:00 +0100

    add docstrings + cleaning

--- src/noon/tries/rythmic_context.clj
+++ src/noon/tries/rythmic_context.clj
@@ -1,69 +1,89 @@
-(ns noon.tries.rythmic-context
-  (:refer-clojure :exclude [cat]))
+(ns noon.tries.rythmic-context)
 
-(def base-layer {:resolution 1 :index 0 :length 1})
+(do :ctx
 
-(defn layer [resolution index length]
-  {:resolution resolution
-   :index index
-   :length length})
+    (defn layer [resolution index length]
+      {:resolution resolution
+       :index index
+       :length length})
 
-(defn mk [& layers]
-  (mapv (fn [[resolution index length]]
-          (layer resolution index length))
-        layers))
+    (defn mk
+      "build a rythmic context given successive `layers`
+       layer are vectors of the form [resolution index length]."
+      [& layers]
+      (mapv (fn [[resolution index length]]
+              (layer resolution index length))
+            layers))
 
-(defn split [ctx format]
-  (let [total (reduce + format)]
-    (map (fn [index length]
-           (conj ctx
-                 {:resolution total
-                  :index index
-                  :length length}))
-         (reductions + 0 format)
-         format)))
+    (defn tup
+      "tupify `ctx` according to given `format`"
+      [ctx format]
+      (let [total (reduce + format)]
+        (map (fn [index length]
+               (conj ctx
+                     {:resolution total
+                      :index index
+                      :length length}))
+             (reductions + 0 format)
+             format)))
 
-(split []
-       [1 2 1])
+    (defn ctx->pos-dur
+      "given `ctx`, returns a vector [start-position duration]"
+      [ctx]
+      (reduce (fn [[pos dur] {:keys [resolution index length]}]
+                (let [unit (/ dur resolution)]
+                  [(+ pos (* index unit))
+                   (* length unit)]))
+              [0 1]
+              ctx))
 
-(defn ctx->pos-dur [ctx]
-  (reduce (fn [[pos dur] {:keys [resolution index length]}]
-            (let [unit (/ dur resolution)]
-              [(+ pos (* index unit))
-               (* length unit)]))
-          [0 1]
-          ctx))
+    (defn compare-ctxs
+      "compare time bounds of `ctx1` and `ctx2`
+       returns a vector of 2 elements such that:
+       [(compare <ctx2-start-pos> <ctx1-start-pos>>)
+        (compare <ctx-1-end-pos> <ctx2-end-pos>>)]"
+      [ctx1 ctx2]
+      (let [[pos1 dur1] (ctx->pos-dur ctx1)
+            [pos2 dur2] (ctx->pos-dur ctx2)]
+        [(compare pos2 pos1)
+         (compare (+ pos1 dur1)
+                  (+ pos2 dur2))]))
 
-(mapv ctx->pos-dur (split []
-                          [1 2 1]))
+    (comment
+      (tup []
+           [1 2 1])
 
-(defn split-score [score format]
-  (mapv (fn [ctx]
-          (set (map (fn [e] (update e :time (fn [t] (into ctx t))))
-                    score)))
-        (split [] format)))
+      (mapv ctx->pos-dur (tup []
+                              [1 2 1]))))
 
-(mapv (fn [score]
-        (set (map #(assoc % :pos-dur (map float (ctx->pos-dur (:time %)))) score)))
-      (split-score (set (map (fn [ctx] {:time ctx})
-                             (split [] [1 2 1])))
-                   [1 2 1]))
+(do :score
 
-(defn compare-ctxs [c1 c2]
-  (let [[pos1 dur1] (ctx->pos-dur c1)
-        [pos2 dur2] (ctx->pos-dur c2)]
-    [(compare pos2 pos1)
-     (compare (+ pos1 dur1)
-              (+ pos2 dur2))]))
+    (defn tup-score
+      "tupify `score` according to `format`"
+      [score format]
+      (set (mapcat (fn [ctx]
+                     (map (fn [e] (update e :time (fn [t] (into ctx t))))
+                          score))
+                   (tup [] format))))
 
-(defn select-subscore [score ctx]
-  (set (remove (fn [event]
-                 (let [[a b] (compare-ctxs ctx (:time event))]
-                   (or (= -1 a) (= -1 b))))
-               score)))
+    (mapv (fn [score]
+            (set (map #(assoc % :pos-dur (map float (ctx->pos-dur (:time %)))) score)))
+          (tup-score (set (map (fn [ctx] {:time ctx})
+                               (tup [] [1 2 1])))
+                     [1 2 1]))
 
-(select-subscore (reduce into #{}
-                         (split-score (set (map (fn [ctx] {:time ctx})
-                                                (split [] [1 2 1])))
-                                      [1 2 1]))
-                 (mk [4 1 2]))
+    (defn split-score
+      "split `score` into 2 scores.
+       the first including events contained in the given `ctx`
+       the second including events outside (or overlapping) `ctx`."
+      [score ctx]
+      (let [grouped (group-by (fn [event]
+                                (let [[a b] (compare-ctxs ctx (:time event))]
+                                  (or (= -1 a) (= -1 b))))
+                              score)]
+        [(set (grouped false)) (set (grouped true))]))
+
+    (split-score (tup-score (set (map (fn [ctx] {:time ctx})
+                                      (tup [] [1 2 1])))
+                            [1 2 1])
+                 (mk [4 1 2])))
